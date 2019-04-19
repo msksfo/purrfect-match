ef382e45657571b634248a42ccf075202f511548
Tia Esguerra <msksfo@gmail.com>
2019-04-18 17:43:59 -0700

    Change spelling of purrfect at Sarah's request

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,15 @@ import '../styles/Header.css';
 
 const Header = () => {
     return (
-        <div className='header-wrappper'>
+        <div className="header-wrappper">
             <h1 className="header">
-                <span className="header--span">Purrrr</span>-Fect Match
+                <span className="header--span">Purr</span>-Fect Match
             </h1>
-            <p className='header--tagline'>Providing loving attention and reliable service for your furrends</p>
+            <p className="header--tagline">
+                Providing loving attention and reliable service for your
+                furrends
+            </p>
         </div>
-        
     );
 };
 
